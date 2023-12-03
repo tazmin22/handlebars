5666bb725c5d49c99892953a66e9c4f1aea45095
tazmin22 <sacaraat@gmail.com>
2023-12-03 08:51:27 -0300

    funcionando

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const handlebars = require ('express-handlebars')
-//const {PManager} = require ("./ProductManager")
-//const productsRouter = require ("./routes/products.router.js")
-//const cartsRouter = require ("./routes/carts.router.js")
+
 
 const app = express()
 const port = 3030
@@ -12,13 +10,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname+'/public'))
 app.engine('handlebars', handlebars.engine())
-app.set('view engine', handlebars)
+app.set('view engine', "handlebars")
 app.set('views', __dirname + '/views')
 
 
-app.get('/views', (req, res) => {
+// app.get('/views', (req, res) => {
+//   res.render('index', {
+//     title: 'e-commerse',
+//     name: 'nombre'
+//   });
+//   app.listen(port)
+// });
+
+
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'e-commerse',
     name: 'nombre'
   });
-});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
